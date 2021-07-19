d0073c50fe580b837712932f25d313d1bb595c01
SiddhiBhand <52746795+SiddhiBhand@users.noreply.github.com>
2021-07-19 20:50:57 +0530

    Add files via upload

---
+++ OOP_Concepts/ConsolForAS/StaticCon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolForAS
+{
+    class StaticCon
+    {
+        static int a = 10;
+        int x = 50;
+        static StaticCon()
+        {
+            a++;
+            Console.WriteLine("value of static:" + a);
+        }
+        public StaticCon(String s)
+        {
+            Console.WriteLine("Name:" + s);
+        }
+        //static void Main(string[] args)
+        //{
+        //    StaticCon obj = new StaticCon("Siddhi");
+        //}
+    }
+}
