c6e24b60d4f588ef6fbae0de5ebdffe05bdb4fd4
SiddhiBhand <52746795+SiddhiBhand@users.noreply.github.com>
2021-07-19 20:47:38 +0530

    polymorphism

---
+++ OOP_Concepts/ConsolForAS/Overriding.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolForAS
+{
+    class Overriding
+    {
+        //static void Main(string[] args)
+        //{
+        //    Bank B1 = new Bank();
+        //    B1.Method();
+        //    Bank B2 = new ICICI();
+        //    B2.Method();
+        //    ICICI B3 = new ICICI();
+        //    B3.Method();
+        //    //ICICI B3 = new Bank(); this will show error
+        //}
+    }
+    class Bank
+    {
+        public virtual float Method()
+        {
+            float ir = 4.5F;
+            Console.WriteLine(ir);
+            return ir;
+        }
+    }
+
+    class ICICI: Bank
+    {
+        public override float Method()
+        {
+            float ir = 4.9F;
+            Console.WriteLine(ir);
+            return ir;
+        }
+    }
+}
---
+++ OOP_Concepts/ConsolForAS/Polymorphism.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolForAS
+{
+    class MultiplyFun
+    {
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+    class Polymorphism
+    {
+        //public static void Main(string[] args)
+        //{
+        //    MultiplyFun M = new MultiplyFun();
+        //    Console.WriteLine(M.Multiply(5, 5));
+        //    Console.WriteLine(M.Multiply(6.5, 4.5));
+        //}
+    }
+}
